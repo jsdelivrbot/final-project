26da9260c45b9d44d62764276afdf873429979fb
Pedro Zurita <pzurita.mail@gmail.com>
2018-07-17 00:34:07 -0500

    Add files via upload

---
+++ src/src/app/components/debt/debt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'debt',
+  templateUrl: './debt.component.html',
+  styleUrls: [ './debt.component.scss' ]
+})
+export class DebtComponent  {
+  name = '';
+}
